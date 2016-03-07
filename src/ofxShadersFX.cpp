ce1f2eda724037e782a2ef3d2eddc92d52c8e830
Alexandre Baron <alexandre.baron@epitech.eu>
2016-03-07 22:24:23 +0100

    Remove unnecessary "this->" (style issue)

--- src/ofxShadersFX.cpp
+++ src/ofxShadersFX.cpp
@@ -1,3 +1,13 @@
+// The MIT License (MIT)
+// Copyright (c) 2016 Alexandre Baron (Scylardor)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 #include "ofxShadersFX.h"
 
 namespace ofxShadersFX {
@@ -29,11 +39,11 @@ namespace ofxShadersFX {
     }
 
     void Shader::reload() {
-        this->m_shader.setupShaderFromSource(GL_VERTEX_SHADER, getShader(GL_VERTEX_SHADER));
-        this->m_shader.setupShaderFromSource(GL_FRAGMENT_SHADER, getShader(GL_FRAGMENT_SHADER));
+        m_shader.setupShaderFromSource(GL_VERTEX_SHADER, getShader(GL_VERTEX_SHADER));
+        m_shader.setupShaderFromSource(GL_FRAGMENT_SHADER, getShader(GL_FRAGMENT_SHADER));
         if(ofIsGLProgrammableRenderer()) {
-            this->m_shader.bindDefaults();
+            m_shader.bindDefaults();
         }
-        this->m_shader.linkProgram();
+        m_shader.linkProgram();
     }
 }
